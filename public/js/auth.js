b3753a66d227bcb265a4fb75adb9cda6a7c5c211
Dave May <echo.dave@protonmail.com>
2019-12-03 23:36:43 -0500

    route logic for handling signups without photo. added remove photo button to form #17

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,105 +1,66 @@
-//on focus remove password warning and if password field, empty it
-$("input").focus(function () {
-    $("#pwdErr").remove();
-    if ($(this).attr("type") == "password") {
-        $(this).val("");
-    }
+$("#imageRemove").on("click", function(e) {
+  $("input[name=photo]").val(null);
 });
 
-$("#signUpForm").on("submit", function (e) {
-    e.preventDefault();
-    //remove input focus
-    $("input").blur();
+$("#signUpForm").on("submit", function(e) {
+  e.preventDefault();
+  //remove input focus
+  $("input").blur();
 
-    /* const files = $("input[name=photo]").prop("files");
+  /* const files = $("input[name=photo]").prop("files");
     const reader = new FileReader();
     console.log(files);
     reader.readAsDataURL(files[0]);
     reader.onload = function () { };
     console.log(this.result); */
 
-    //        see if pass word fields match
-    if ($("input[name=pass").val() == $("input[name=passTest]").val()) {
-        console.log("validated");
-        // let form = document.forms.namedItem("signUpForm")
-        let form = document.querySelector("#signUpForm");
-        // let form = document.forms;
-        console.log(form);
+  //        see if pass word fields match
+  if ($("input[name=pass").val() == $("input[name=passTest]").val()) {
+    console.log("validated");
+    // let form = document.forms.namedItem("signUpForm")
+    let form = document.querySelector("#signUpForm");
+    // let form = document.forms;
+    console.log(form);
 
-        /*       const inputs = $('input[name!=passTest]').serializeArray();
+    /*       const inputs = $('input[name!=passTest]').serializeArray();
                   console.log('inputs');
                   console.log(inputs); */
 
-        let newInputs = new FormData(form);
-        console.log("form data");
+    let newInputs = new FormData(form);
+    console.log("form data");
 
-        //formData.append($("input[name!=passTest]").val());
-        console.log(newInputs);
-        //logging of FormData
-        for (var [key, value] of newInputs.entries()) {
-            console.log(key, value);
-        }
-
-        $.ajax({
-            url: '/api/auth/newuser',
-            data: newInputs,
-            processData: false,
-            contentType: false,
-            type: 'POST',
-        }).then(function (res) {
-            console.log(res);
-
-        })
-
-        // $.post("/api/auth/newuser", newInputs, function (res) {
-        //     console.log(res);
-        // });
-
-
-    } else {
-        $("label[for=passTest").before(
-            `<span id="pwdErr"> Password mismatch</span>`
-        );
+    //formData.append($("input[name!=passTest]").val());
+    console.log(newInputs);
+    //logging of FormData
+    for (var [key, value] of newInputs.entries()) {
+      console.log(key, value);
     }
+
+    $.ajax({
+      url: "/api/auth/newuser",
+      data: newInputs,
+      processData: false,
+      contentType: false,
+      type: "POST"
+    }).then(function(res) {
+      if (res.redirect) {
+        console.log("is redirect");
+
+        window.location = res.redirect;
+      }
+    });
+  } else {
+    $("label[for=passTest").before(
+      `<span id="pwdErr"> Password mismatch</span>`
+    );
+  }
 });
 
-//make new user
-// $('button[name=makeNewUser]').on('click', function (event) {
+$("#loginForm").on("submit", function(event) {
+  event.preventDefault();
+  console.log($(this).serialize());
 
-//     // event.preventDefault();
-//     //remove input focus
-//     $('input').blur();
-//     // see if pass word fields match
-//     if ($('input[name=pass').val() == $('input[name=passTest]').val()) {
-//         console.log('true and posting');
-//         //build array of object key value pairs excluding passTest
-//         let fileUpload = $('input[type=file]').val()
-//         console.log(fileUpload);
-
-//         $.post('/upload', fileUpload, function (data) {
-//             console.log('upload response');
-//             console.log(data);
-
-//         })
-
-//         const inputs = $('input[name!=passTest]').serializeArray();
-//         console.log('inputs');
-//         console.log(inputs);
-
-//         $.post('/api/auth/newuser', inputs, function (res) {
-//             console.log(res);
-//         })
-//     } else {
-//         $('label[for=passTest').before(`<span id="pwdErr"> Password mismatch</span>`)
-//     }
-
-// })
-
-$("#loginForm").on("submit", function (event) {
-    event.preventDefault();
-    console.log($(this).serialize());
-
-    $.post("/api/auth/login", $(this).serialize(), function (res) {
-        console.log(res);
-    });
+  $.post("/api/auth/login", $(this).serialize(), function(res) {
+    console.log(res);
+  });
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,74 +1,95 @@
 const db = require("../database/models");
 const path = require("path");
-module.exports = function (app) {
-  app.get("/api/users", function (req, res) {
-    db.User.findAll().then(function (data) {
+module.exports = function(app) {
+  app.get("/api/users", function(req, res) {
+    db.User.findAll().then(function(data) {
       res.json(data);
     });
   });
-  app.post("/api/auth/newuser", function (req, res) {
+  app.post("/api/auth/newuser", function(req, res) {
+    console.log(req);
+    console.log("body---------------");
     console.log(req.body);
-    console.log("file--------------file");
 
-    console.log(req.files);
-
-    req.files.photo.mv(path.join(__dirname, '../public/upload', req.files.photo.name.slice(0, -4) + Date.now() + req.files.photo.name.slice(-4)), function (err) {
-      if (err) {
-        console.log(err);
-        res.send(err);
-      } else {
-        req.files.photo.namelong = req.files.photo.name.slice(0, -4) + Date.now() + req.files.photo.name.slice(-4);
-        console.log('upload success');
-        //return res.send('success: ' + req.files.photo.name);
-      }
-    });
     console.log("create--------------create");
 
-    db.User.create(req.body).then(function (data) {
-      console.log("user ------------->");
-      console.log(data);
-      req.session.user = data.dataValues;
-
-      console.log("----------");
-      console.log("session");
-      console.log(req.session);
-      res.send("success");
-      //req.session.user = user.dataValues;
-      //res.redirect("/");
-      return data
-      // return res.send("session started");
-    }).then(
-      function (data) {
-        console.log('creat then log data');
+    db.User.create(req.body)
+      .then(function(data) {
+        console.log("user ------------->");
         console.log(data);
-        console.log('file name------------>');
-        console.log(req.files.photo.name);
 
-        db.Media.create({
-          url: "/upload/" + req.files.photo.namelong,
-          caption: req.files.photo.name.slice(0, -4),
-          UserId: data.dataValues.id,
-          media_type: req.files.photo.name.slice(-4)
-        }).then(function (media) {
-          db.User.update({
-            photo: media.dataValues.id
-          }, {
-            where: { id: media.dataValues.UserId }
+        //make a login session
+        req.session.user = data.dataValues;
 
-
-          })
-        })
-
+        console.log("----------");
+        console.log("session");
+        console.log(req.session);
+        //res.send("success");
+        //res.redirect("/");
+        return data;
+        // return res.send("session started");
       })
+      .then(function(data) {
+        console.log("creat then log data");
+        console.log(data);
+        console.log("file name------------>");
+        //console.log(req.files.photo.name);
+        if (req.files != null) {
+          console.log("file--------------file");
+
+          console.log(req.files);
+
+          req.files.photo.mv(
+            path.join(
+              __dirname,
+              "../public/upload",
+              req.files.photo.name.slice(0, -4) +
+                Date.now() +
+                req.files.photo.name.slice(-4)
+            ),
+            function(err) {
+              if (err) {
+                console.log(err);
+                res.send(err);
+              } else {
+                req.files.photo.namelong =
+                  req.files.photo.name.slice(0, -4) +
+                  Date.now() +
+                  req.files.photo.name.slice(-4);
+                console.log("upload success");
+              }
+            }
+          );
+
+          db.Media.create({
+            url: "/upload/" + req.files.photo.namelong,
+            caption: req.files.photo.name.slice(0, -4),
+            UserId: data.dataValues.id,
+            media_type: req.files.photo.name.slice(-4)
+          }).then(function(media) {
+            db.User.update(
+              {
+                photo: media.dataValues.id
+              },
+              {
+                where: { id: media.dataValues.UserId }
+              }
+            );
+            res.send({ redirect: "/" });
+          });
+        } else {
+          res.send({ redirect: "/" });
+        }
+      });
 
     //res.json();
   });
-  app.post("/api/auth/login", function (req, res) {
+  app.post("/api/auth/login", function(req, res) {
     console.log(req.body);
 
     db.User.findOne({
       where: { name: req.body.name }
-    }).then(async function (user) {
+    }).then(async function(user) {
       if (await user.validPassword(req.body.pass)) {
         console.log("success");
         res.send("success");
@@ -80,22 +101,32 @@ module.exports = function (app) {
     });
   });
 
-  app.post("/upload", function (req, res) {
+  app.post("/upload", function(req, res) {
     console.log("file upoad route--------------------");
 
     console.log(req.files); // the uploaded file object
-    req.files.photo.mv(path.join(__dirname, '../public/upload', req.files.photo.name.slice(0, -4) + Date.now() + req.files.photo.name.slice(-4)), function (err) {
-      if (err) {
-        console.log(err);
-        res.send(err);
-
-      } else {
-        req.files.photo.name = req.files.photo.name.slice(0, -4) + Date.now() + req.files.photo.name.slice(-4);
-        console.log('upload success');
-        return res.send('success: ' + req.files.photo.name);
-
+    req.files.photo.mv(
+      path.join(
+        __dirname,
+        "../public/upload",
+        req.files.photo.name.slice(0, -4) +
+          Date.now() +
+          req.files.photo.name.slice(-4)
+      ),
+      function(err) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          req.files.photo.name =
+            req.files.photo.name.slice(0, -4) +
+            Date.now() +
+            req.files.photo.name.slice(-4);
+          console.log("upload success");
+          return res.send("success: " + req.files.photo.name);
+        }
       }
-    });
+    );
     // res.json();
   });
 };
