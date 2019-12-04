83a91b99cb83058005b67face1c0e80afafe41ba
Triston Tetley <tetleylax@aol.com>
2019-12-04 13:52:01 -0500

    working cookie sessions

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,49 +1,45 @@
-
 //on focus remove password warning and if password field, empty it
-$('input').focus(function () {
-    $('#pwdErr').remove();
-    if ($(this).attr('type') == 'password') {
-        $(this).val('');
-    }
-})
-$('button[name=makeNewUser]').on('click', function (event) {
+$("input").focus(function() {
+  $("#pwdErr").remove();
+  if ($(this).attr("type") == "password") {
+    $(this).val("");
+  }
+});
+$("button[name=makeNewUser]").on("click", function(event) {
+  // event.preventDefault();
+  //remove input focus
+  $("input").blur();
+  // see if pass word fields match
+  if ($("input[name=pass").val() == $("input[name=passTest]").val()) {
+    console.log("true and posting");
+    //build array of object key value pairs excluding passTest
+    let fileUpload = $("input[type=file]").val();
+    console.log(fileUpload);
 
-    // event.preventDefault();
-    //remove input focus
-    $('input').blur();
-    // see if pass word fields match
-    if ($('input[name=pass').val() == $('input[name=passTest]').val()) {
-        console.log('true and posting');
-        //build array of object key value pairs excluding passTest
-        let fileUpload = $('input[type=file]').val()
-        console.log(fileUpload);
+    $.post("/upload", fileUpload, function(data) {
+      console.log("upload response");
+      console.log(data);
+    });
 
-        $.post('/upload', fileUpload, function (data) {
-            console.log('upload response');
-            console.log(data);
+    const inputs = $("input[name!=passTest]").serializeArray();
+    console.log("inputs");
+    console.log(inputs);
 
+    $.post("/api/auth/newuser", inputs, function(res) {
+      console.log(res);
+    });
+  } else {
+    $("label[for=passTest").before(
+      `<span id="pwdErr"> Password mismatch</span>`
+    );
+  }
+});
 
-        })
+$("#loginForm").on("submit", function(event) {
+  event.preventDefault();
+  console.log($(this).serialize());
 
-        const inputs = $('input[name!=passTest]').serializeArray();
-        console.log('inputs');
-        console.log(inputs);
-
-        $.post('/api/auth/newuser', inputs, function (res) {
-            console.log(res);
-        })
-    } else {
-        $('label[for=passTest').before(`<span id="pwdErr"> Password mismatch</span>`)
-    }
-
-})
-
-$('#loginForm').on('submit', function (event) {
-    event.preventDefault();
-    console.log($(this).serialize());
-
-    $.post('/api/auth/login', $(this).serialize(), function (res) {
-        console.log(res);
-
-    })
-})
+  $.post("/api/auth/login", $(this).serialize(), function(res) {
+    console.log(res);
+  });
+});
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,10 @@
-const path = require('path');
-module.exports = function (app) {
-
-app.get(app.get(/(\/index)|(\/home)|(^\/$)/, function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-}));
+const path = require("path");
+module.exports = function(app) {
+  app.get(
+    app.get(/(\/index)|(\/home)|(^\/$)/, function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+      console.log("page loaded-----------------");
+      console.log(req.session);
+    })
+  );
 };
