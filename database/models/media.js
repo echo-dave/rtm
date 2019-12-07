ba560a9d6e46f01b66a4c40fc87f68bf2390d546
Dave May <echo.dave@protonmail.com>
2019-12-06 21:43:51 -0500

    revert model changes crashing deployment

--- database/models/media.js
+++ database/models/media.js
@@ -1,24 +1,17 @@
 module.exports = function(sequelize, dataTypes) {
-  const Media = sequelize.define(
-    "Media",
-    {
-      caption: {
-        type: dataTypes.STRING,
-        defaultValue: null
-      },
-      media_type: {
-        type: dataTypes.STRING,
-        allowNull: false
-      },
-      url: {
-        type: dataTypes.STRING
-      }
+  const Media = sequelize.define("Media", {
+    caption: {
+      type: dataTypes.STRING,
+      defaultValue: null
     },
-    {
-      charset: "utf8",
-      collate: "utf8mb4_unicode_520_ci"
+    media_type: {
+      type: dataTypes.STRING,
+      allowNull: false
+    },
+    url: {
+      type: dataTypes.STRING
     }
-  );
+  });
 
   return Media;
 };
